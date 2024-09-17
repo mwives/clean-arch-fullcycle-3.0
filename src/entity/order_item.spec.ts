f0f3324b91b5392666075e5a4da966e3f36ec7d8
Ives M. W <ivesmw@gmail.com>
2024-09-17 11:11:10 -0300

    refactor: encapsulate private properties in OrderItem class and add validation

---
+++ src/entity/order_item.spec.ts
@@ -0,0 +1,38 @@
+import { OrderItem } from './order_item'
+
+describe('OrderItem', () => {
+  it('should throw error when creating an order item without ID', () => {
+    expect(() => new OrderItem('', 'p1', 'Product 1', 10, 1)).toThrow(
+      'ID is required'
+    )
+  })
+
+  it('should throw error when creating an order item without product ID', () => {
+    expect(() => new OrderItem('123', '', 'Product 1', 10, 1)).toThrow(
+      'Product ID is required'
+    )
+  })
+
+  it('should throw error when creating an order item without name', () => {
+    expect(() => new OrderItem('123', 'p1', '', 10, 1)).toThrow(
+      'Name is required'
+    )
+  })
+
+  it('should throw error when creating an order item with price less than or equal to 0', () => {
+    expect(() => new OrderItem('123', 'p1', 'Product 1', 0, 1)).toThrow(
+      'Price must be greater than 0'
+    )
+  })
+
+  it('should throw error when creating an order item with quantity less than or equal to 0', () => {
+    expect(() => new OrderItem('123', 'p1', 'Product 1', 10, 0)).toThrow(
+      'Quantity must be greater than 0'
+    )
+  })
+
+  it('should calculate order item total', () => {
+    const orderItem = new OrderItem('123', 'p1', 'Product 1', 10, 2)
+    expect(orderItem.orderItemTotal()).toBe(20)
+  })
+})
--- src/entity/order_item.ts
+++ src/entity/order_item.ts
@@ -1,15 +1,52 @@
 export class OrderItem {
-  _id: string
-  _name: string
-  _price: number
+  private _id: string
+  private _productId: string
+  private _name: string
+  private _price: number
+  private _quantity: number
 
-  constructor(id: string, name: string, price: number) {
+  constructor(
+    id: string,
+    productId: string,
+    name: string,
+    price: number,
+    quantity: number
+  ) {
     this._id = id
+    this._productId = productId
     this._name = name
     this._price = price
+    this._quantity = quantity
+    this.validate()
   }
 
   get price() {
     return this._price
+  }
+
+  get quantity() {
+    return this._quantity
+  }
+
+  validate() {
+    if (!this._id) {
+      throw new Error('ID is required')
+    }
+    if (!this._productId) {
+      throw new Error('Product ID is required')
+    }
+    if (!this._name) {
+      throw new Error('Name is required')
+    }
+    if (this._price <= 0) {
+      throw new Error('Price must be greater than 0')
+    }
+    if (this._quantity <= 0) {
+      throw new Error('Quantity must be greater than 0')
+    }
+  }
+
+  orderItemTotal() {
+    return this._price * this._quantity
   }
 }
