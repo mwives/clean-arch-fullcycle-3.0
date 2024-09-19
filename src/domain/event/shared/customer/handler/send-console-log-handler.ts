06fd25bcd22aaa2050a37bf6f6e7829ec3045e0a
Ives M. W <ivesmw@gmail.com>
2024-09-19 20:00:30 -0300

    refactor: add generic to EventInterface eventData type and fix console.log

--- src/domain/event/shared/customer/handler/send-console-log-handler.ts
+++ src/domain/event/shared/customer/handler/send-console-log-handler.ts
@@ -1,10 +1,12 @@
+import { Customer } from '../../../../entity/customer'
 import { EventHandlerInterface } from '../../event-handler.interface'
 import { EventInterface } from '../../event.interface'
 
 export class SendConsoleLogHandler implements EventHandlerInterface {
-  handle(event: EventInterface): void {
+  handle(event: EventInterface<Customer>): void {
     console.log(
-      `Client address: ${event.eventData.id}, ${event.eventData.name} changed to: ${event.eventData.address}`
+      `Client address: ${event.eventData.id}, 
+      ${event.eventData.name} changed to: ${event.eventData.Address.toString()}`
     )
   }
 }
