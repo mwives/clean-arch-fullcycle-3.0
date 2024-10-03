9a523b604e7bafaaa1c81868549343fc30ab4f8a
Ives M. W <ivesmw@gmail.com>
2024-10-03 10:48:25 -0300

    refactor: update ProductRepositoryInterface to use ProductInterface

--- src/domain/product/repository/product-repository.interface.ts
+++ src/domain/product/repository/product-repository.interface.ts
@@ -1,5 +1,5 @@
-import { Product } from '@product/entity/product'
+import { ProductInterface } from '@product/entity/product.interface'
 import { RepositoryInterface } from '@shared/repository/repository-interface'
 
 export interface ProductRepositoryInterface
-  extends RepositoryInterface<Product> {}
+  extends RepositoryInterface<ProductInterface> {}
