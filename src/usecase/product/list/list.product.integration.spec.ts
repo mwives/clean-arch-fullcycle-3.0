e8c23b80363cf49bf9e5b25b40514b1c9d95e6c5
Ives M. W <ivesmw@gmail.com>
2024-10-03 12:12:58 -0300

    fix: integration tests for ListProductUseCase

--- src/usecase/product/list/list.product.integration.spec.ts
+++ src/usecase/product/list/list.product.integration.spec.ts
@@ -1,7 +1,10 @@
 import { ProductModel } from '@infra/product/repository/sequelize/product.model'
+import { ProductRepository } from '@infra/product/repository/sequelize/product.repository'
+import { ProductFactory } from '@product/factory/product.factory'
 import { Sequelize } from 'sequelize-typescript'
+import { ListProductUseCase } from './list.product.usecase'
 
-describe('List Product Integration Test', () => {
+describe('ListProductUseCase Integration Test', () => {
   let sequelize: Sequelize
 
   beforeEach(async () => {
@@ -22,14 +25,21 @@ describe('List Product Integration Test', () => {
 
   describe('execute', () => {
     it('should list products', async () => {
-      const product = ProductModel.build({ name: 'any_name', price: 9.9 })
-      await product.save()
+      const product1 = ProductFactory.create('a', 'any_name1', 9.9)
+      const product2 = ProductFactory.create('b', 'any_name2', 9.9)
 
-      const products = await ProductModel.findAll()
+      const productRepository = new ProductRepository()
+      await productRepository.create(product1)
+      await productRepository.create(product2)
 
-      expect(products).toHaveLength(1)
-      expect(products[0].name).toBe('any_name')
-      expect(products[0].price).toBe(9.9)
+      const useCase = new ListProductUseCase(productRepository)
+      const output = await useCase.execute({})
+
+      expect(output.products).toHaveLength(2)
+      expect(output.products[0].name).toBe('any_name1')
+      expect(output.products[0].price).toBe(9.9)
+      expect(output.products[1].name).toBe('any_name2')
+      expect(output.products[1].price).toBe(19.8)
     })
   })
 })
