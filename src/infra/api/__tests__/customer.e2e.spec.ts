c1d1f288706b7179de2e6bb621653bc44b7138c3
Ives M. W <ivesmw@gmail.com>
2024-10-04 13:59:24 -0300

    refactor: move tests to describe

--- src/infra/api/__tests__/customer.e2e.spec.ts
+++ src/infra/api/__tests__/customer.e2e.spec.ts
@@ -10,10 +10,23 @@ describe('Customer E2E', () => {
     await sequelize.close()
   })
 
-  it('should create a customer', async () => {
-    const response = await request(app)
-      .post('/customers')
-      .send({
+  describe('POST /customers', () => {
+    it('should create a customer', async () => {
+      const response = await request(app)
+        .post('/customers')
+        .send({
+          name: 'any_name',
+          address: {
+            street: 'any_street',
+            number: 1,
+            city: 'any_city',
+            zip: 'any_zip',
+          },
+        })
+
+      expect(response.status).toBe(201)
+      expect(response.body).toEqual({
+        id: expect.any(String),
         name: 'any_name',
         address: {
           street: 'any_street',
@@ -22,23 +35,12 @@ describe('Customer E2E', () => {
           zip: 'any_zip',
         },
       })
-
-    expect(response.status).toBe(201)
-    expect(response.body).toEqual({
-      id: expect.any(String),
-      name: 'any_name',
-      address: {
-        street: 'any_street',
-        number: 1,
-        city: 'any_city',
-        zip: 'any_zip',
-      },
     })
-  })
 
-  it('should not create a customer with invalid data', async () => {
-    const response = await request(app).post('/customers').send({})
+    it('should not create a customer with invalid data', async () => {
+      const response = await request(app).post('/customers').send({})
 
-    expect(response.status).toBe(500)
+      expect(response.status).toBe(500)
+    })
   })
 })
