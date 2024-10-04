ad7e144db1502ec837f6caff005e85f69339c5f3
Ives M. W <ivesmw@gmail.com>
2024-10-04 14:05:47 -0300

    feat: add GET /customers route and e2e test

--- src/infra/api/__tests__/customer.e2e.spec.ts
+++ src/infra/api/__tests__/customer.e2e.spec.ts
@@ -43,4 +43,46 @@ describe('Customer E2E', () => {
       expect(response.status).toBe(500)
     })
   })
+
+  describe('GET /customers', () => {
+    it('should list all customers', async () => {
+      const createResponse1 = await request(app)
+        .post('/customers')
+        .send({
+          name: 'any_name1',
+          address: {
+            street: 'any_street',
+            number: 1,
+            city: 'any_city',
+            zip: 'any_zip',
+          },
+        })
+
+      expect(createResponse1.status).toBe(201)
+
+      const createResponse2 = await request(app)
+        .post('/customers')
+        .send({
+          name: 'any_name2',
+          address: {
+            street: 'any_street',
+            number: 1,
+            city: 'any_city',
+            zip: 'any_zip',
+          },
+        })
+
+      expect(createResponse2.status).toBe(201)
+
+      const response = await request(app).get('/customers')
+
+      expect(response.status).toBe(200)
+      expect(response.body.customers.length).toBe(2)
+      const [customer1, customer2] = response.body.customers
+      expect(customer1.name).toBe('any_name1')
+      expect(customer1.address.street).toBe('any_street')
+      expect(customer2.name).toBe('any_name2')
+      expect(customer2.address.street).toBe('any_street')
+    })
+  })
 })
--- src/infra/api/routes/customer.route.ts
+++ src/infra/api/routes/customer.route.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import { CustomerRepository } from '@infra/customer/repository/sequelize/customer.repository'
 import { CreateCustomerUseCase } from '@usecase/customer/create/create.customer.usecase'
 import { InputCreateCustomerDto } from '@usecase/customer/create/create.customer.dto'
+import { ListCustomerUseCase } from '@usecase/customer/list/list.customer.usecase'
 
 export const customerRouter = express.Router()
 
@@ -23,6 +24,18 @@ customerRouter.post('/', async (req, res) => {
     const output = await usecase.execute(customerDto)
 
     res.status(201).json(output)
+  } catch (err) {
+    res.status(500).send('Internal server error')
+  }
+})
+
+customerRouter.get('/', async (req, res) => {
+  try {
+    const useCase = new ListCustomerUseCase(new CustomerRepository())
+
+    const customers = await useCase.execute({})
+
+    res.status(200).json(customers)
   } catch (err) {
     res.status(500).send('Internal server error')
   }
